8b3aad2e59e189e8f50419acb9a8b3466772a713
piercepatrick <piercepatrick2001@gmail.com>
2021-05-13 11:11:01 -0500

    Get weather data from json

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const apiKey = 'a40b56249b0a84a90a3e822cdb0aee31'\r\nasync function callAPI(city) {\r\n    const response = await fetch(`//api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`, {mode: 'cors'})\r\n    const data = await response.json()\r\n    console.log(data)\r\n}\r\n\r\n\r\n\r\ncallAPI('London')\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("const apiKey = 'a40b56249b0a84a90a3e822cdb0aee31'\r\nasync function callAPI(city) {\r\n    const response = await fetch(`//api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`, {mode: 'cors'})\r\n    const data = await response.json()\r\n    processData(data)\r\n}\r\n\r\nfunction processData(apiData) {\r\n    const weatherData = {name: apiData.name, temp: apiData.main.temp, tempFeelsLike: apiData.main.feels_like, humidity: apiData.main.humidity, tempHigh: apiData.main.temp_max,\r\n                        tempLow: apiData.main.temp_min, weather: apiData.weather[0].main, weatherDescription: apiData.weather[0].description, pressure: apiData.main.pressure,\r\n                        windSpeed: apiData.wind.speed}\r\n    return weatherData\r\n}\r\ncallAPI('London')\r\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
